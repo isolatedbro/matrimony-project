706e6e8d06f72571c7632630b0b958f392075917
nonha <manish06598@gmail.com>
2026-07-16 02:22:30 +0530

    jlkfd

--- src/routes/Auth/Authentication.jsx
+++ src/routes/Auth/Authentication.jsx
@@ -5,11 +5,11 @@ const Authentcation = () => {
   const navigate = useNavigate();
   const handleButtonClick = (e) => {
     if (e.target.textContent.toLowerCase() === "login") {
-      window.location.href ="matrimony-project/auth/login";
+      window.location.href ="/auth/login";
       // navigate("/auth/login");
     } else if (e.target.textContent.toLowerCase() === "register") {
       // navigate("/auth/registration");
-      window.location.href = "matrimony-project/auth/registration";
+      window.location.href = "/auth/registration";
     }
   };
   return (
